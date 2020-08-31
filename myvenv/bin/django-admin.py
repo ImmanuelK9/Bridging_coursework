e48486833b4952ac71c35e816b4450e05c935729
ImmanuelK9 <pauletarita@gmail.com>
2020-08-31 23:24:23 +0300

    first commit from tutorial

---
+++ myvenv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/media/paulos/Storage/Files/_Uni/Bridging_coursework/myvenv/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
